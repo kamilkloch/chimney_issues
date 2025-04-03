6390b0e47005ff3f1cffb61bf9b3f2e3c473d8d6
Kamil Kloch <kamil.kloch@superfund-technologies.com>
2025-04-03 12:33:37 +0200

    Simplified Java enums.

--- module2/src/main/java/p2/Side.java
+++ module2/src/main/java/p2/Side.java
@@ -2,30 +2,7 @@ package p2;
 
 public enum Side
 {
-    BUY((short)1),
+    BUY,
 
-    SELL((short)2);
-
-    private final short value;
-
-    Side(final short value)
-    {
-        this.value = value;
-    }
-
-    public short value()
-    {
-        return value;
-    }
-
-    public static Side get(final short value)
-    {
-        switch (value)
-        {
-            case 1: return BUY;
-            case 2: return SELL;
-        }
-
-        throw new IllegalArgumentException("Unknown value: " + value);
-    }
+    SELL;
 }

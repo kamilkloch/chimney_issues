d97946d9cf6309a7271cdc242966f3880c40b94c
Kamil Kloch <kamil.kloch@superfund-technologies.com>
2025-04-08 11:58:59 +0200

    Simplified Java enums.

--- module3/src/main/scala/TestTransformer.scala
+++ module3/src/main/scala/TestTransformer.scala
@@ -1,13 +1,14 @@
 import io.scalaland.chimney.dsl.*
 
 object TestTransformer {
-
   def main(args: Array[String]): Unit = {
-//    val s1: p1.Side = p1.Side.BUY
-//    println(s1.into[p2.Side].enableMacrosLogging.transform)
+    val s1: p1.Side = p1.Side.BUY
+    
+    p2.Side.SELL // <- commenting out this line breaks compilation
+    println(s1.into[p2.Side].enableMacrosLogging.transform)
 
     // transformation within the same module works fine
-    val ss1: pp1.Side = pp1.Side.BUY
-    println(ss1.into[pp2.Side].enableMacrosLogging.transform)
+//    val ss1: pp1.Side = pp1.Side.BUY
+//    println(ss1.into[pp2.Side].enableMacrosLogging.transform)
   }
 }
